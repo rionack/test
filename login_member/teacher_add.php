159c8966d9e19d90f3c4969780a2b9f4a711d58d
ryoichi <ryo.nakamura1@gmail.com>
2023-06-27 14:35:56 +0900

    ログインセッションを追加

--- login_member/teacher_add.php
+++ login_member/teacher_add.php
@@ -1,3 +1,16 @@
+<?php
+session_start ();
+session_regenerate_id(true);
+if(isset($_SESSION['login']) == false){
+  print 'ログインされていません';
+  print '<a href="teacher_login.html">ログイン画面へ</a>';
+  exit();
+} else{
+  print $_SESSION['teacher_name'];
+  print 'さんがログイン中<br>';
+}
+ ?>
+
 <!DOCTYPE html>
 <html>
 <head>
@@ -11,7 +24,7 @@
 先生の名前を入れてください<br>
 <input name="name" type="text" style="width:200px"><br>
 先生のメールアドレスを入れてください<br>
-<input name="email" type="text" style="width:200px"><br> 
+<input name="email" type="text" style="width:200px"><br>
 パスワードを入力してください<br>
 <input name="pass" type="password" style="width:100px"><br>
 パスワードをもう一度入れてください<br>
