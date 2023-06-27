552eb687c66446327aaa7e82707071a121ed4daf
ryoichi <ryo.nakamura1@gmail.com>
2023-06-27 14:47:19 +0900

    呼称を変更

---
+++ login_member/teacher_ng.php
@@ -0,0 +1,25 @@
+<?php
+session_start ();
+session_regenerate_id(true);
+if(isset($_SESSION['login']) == false){
+  print 'ログインされていません';
+  print '<a href="teacher_login.html">ログイン画面へ</a>';
+  exit();
+} else{
+  print $_SESSION['teacher_name'];
+  print '先生がログイン中<br>';
+}
+ ?>
+
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>成績管理アプリ</title>
+</head>
+<body>
+先生が選択されていません<br>
+<a href="teacher_list.php">戻る</a>
+
+</body>
+</html>
