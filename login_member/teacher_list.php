ab04d9dcb3ed15c2ae668bd5077c811ccc6c0099
ryoichi <ryo.nakamura1@gmail.com>
2023-06-27 14:10:21 +0900

    コピペ&改造

---
+++ login_member/teacher_list.php
@@ -0,0 +1,56 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>成績管理アプリ</title>
+</head>
+<body>
+<?php
+  try{
+
+    $dsn = 'mysql:dbname=test;host=localhost;charset=utf8';
+    $user = 'root';
+    $password = 'root';
+    $dbh = new PDO($dsn, $user, $password);
+    $dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+    $sql = 'SELECT id, name, email FROM login_member WHERE 1';
+    $stmt = $dbh->prepare($sql);
+    $stmt->execute();
+
+    $dbh = null;
+
+    print '登録済み先生一覧<br><br>';
+
+    print '<form method="post" action="teacher_branch.php">';
+
+    while(true){
+      $rec = $stmt->fetch(PDO::FETCH_ASSOC);
+
+      if($rec == false){
+        break;
+      }
+    print '<input type="radio" name="teachercode" value="'.$rec['id'].'">';
+    print $rec['name']. '先生　：';
+    print $rec['email'];
+    print '<br>';
+    }
+    print '<br>';
+    print '<input type="submit" name="disp" value="参照">';
+    print '<input type="submit" name="add" value="追加">';
+    print '<input type="submit" name="edit" value="修正">';
+    print '<input type="submit" name="delete" value="削除">';
+    print '</form>';
+
+
+}
+catch(Exception $e){
+      print 'ただいま障害による大変ご迷惑をおかけします';
+      exit();
+}
+ ?>
+<br>
+<a href="teacher_login.html">ログイン画面へ</a><br>
+
+</body>
+</html>
