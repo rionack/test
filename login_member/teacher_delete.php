a78b6a459362fcabd075561e1c1967da20b8088b
ryoichi <ryo.nakamura1@gmail.com>
2023-06-27 21:21:09 +0900

    改造

--- login_member/teacher_delete.php
+++ login_member/teacher_delete.php
@@ -3,11 +3,11 @@ session_start ();
 session_regenerate_id(true);
 if(isset($_SESSION['login']) == false){
   print 'ログインされていません';
-  print '<a href="../staff_login/staff_login.html">ログイン画面へ</a>';
+  print '<a href="teacher_login.html">ログイン画面へ</a>';
   exit();
 } else{
-  print $_SESSION['staff_name'];
-  print 'さんがログイン中<br>';
+  print $_SESSION['teacher_name'];
+  print '先生がログイン中<br>';
 }
  ?>
 
@@ -15,27 +15,27 @@ if(isset($_SESSION['login']) == false){
 <html>
 <head>
 <meta charset="UTF-8">
-<title>ろくまる農園</title>
+<title>成績管理アプリ</title>
 </head>
 <body>
 <?php
 try{
 
-  $staff_code = $_GET['staffcode'];
+  $teacher_id = $_GET['teachercode'];
 
-  $dsn ='mysql:dbname=shop;host=localhost;charset=utf8';
+  $dsn ='mysql:dbname=test;host=localhost;charset=utf8';
   $user = 'root';
   $password = 'root';
   $dbh = new PDO($dsn, $user, $password);
   $dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
-  $sql = 'SELECT name FROM mst_staff WHERE code=?';
+  $sql = 'SELECT name FROM login_member WHERE id=?';
   $stmt = $dbh->prepare($sql);
-  $data[] = $staff_code;
+  $data[] = $teacher_id;
   $stmt->execute($data);
 
   $rec = $stmt->fetch(PDO::FETCH_ASSOC);
-  $staff_name = $rec['name'];
+  $teacher_name = $rec['name'];
 
   $dbh = null;
 }
@@ -45,17 +45,17 @@ catch(Exeption $e){
 }
  ?>
 
-スタッフ削除<br>
+登録済みの先生を削除<br>
 <br>
-スタッフコード<br>
-<?php print $staff_code; ?>
+先生ID<br>
+<?php print $teacher_id; ?>
 <br>
-スタッフ名<br>
-<?php print $staff_name; ?>
+先生名<br>
+<?php print $teacher_name; ?>
 <br>
-このスタッフを削除してよろしいですか？<br>
-<form method="post" action="staff_delete_done.php">
-<input type="hidden" name="code" value="<?php print $staff_code; ?>">
+この先生を削除してよろしいですか？<br>
+<form method="post" action="teacher_delete_done.php">
+<input type="hidden" name="id" value="<?php print $teacher_id; ?>">
 
 <input type="button" onclick="history.back()" value="戻る">
 <input type="submit" value="OK">
