f56034b80e1b4d5eff644757f91b50c342d0ac80
ryoichi <ryo.nakamura1@gmail.com>
2023-06-27 21:29:58 +0900

    自動ログアウトを改造

--- login_member/teacher_auto_logout.php
+++ login_member/teacher_auto_logout.php
@@ -5,19 +5,5 @@ if(isset($_COOKIE[session_name()]) == true){
   setcookie(session_name(), '', time()-42000, '/');
 }
 session_destroy();
+header('Location:teacher_login.html');
  ?>
-
-<!DOCTYPE html>
-<html>
-<head>
-<meta charset="UTF-8">
-<title>成績管理アプリ</title>
-</head>
-<body>
-ログアウトしました<br>
-<br>
-<a href="teacher_login.html">ログイン画面へ</a><br>
-
-
-</body>
-</html>
