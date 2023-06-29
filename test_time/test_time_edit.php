61efede471a919bf4b5a3c4616a70d3e9638a1d9
ryoichi <ryo.nakamura1@gmail.com>
2023-06-29 10:46:08 +0900

    改造

--- test_time/test_time_edit.php
+++ test_time/test_time_edit.php
@@ -3,49 +3,43 @@ session_start ();
 session_regenerate_id(true);
 if(isset($_SESSION['login']) == false){
   print 'ログインされていません';
-  print '<a href="../staff_login/staff_login.html">ログイン画面へ</a>';
+  print '<a href="../login_member/teacher_login.html">ログイン画面へ</a>';
   exit();
 } else{
-  print $_SESSION['staff_name'];
-  print 'さんがログイン中<br>';
+  print $_SESSION['teacher_name'];
+  print '先生がログイン中<br>';
 }
+ require_once('../common/common.php')
  ?>
 
 <!DOCTYPE html>
 <html>
 <head>
 <meta charset="UTF-8">
-<title>ろくまる農園</title>
+<title>成績管理アプリ</title>
 </head>
 <body>
 <?php
 try{
 
-  $pro_code = $_GET['procode'];
+  $test_id = $_GET['testid'];
 
-  $dsn ='mysql:dbname=shop;host=localhost;charset=utf8';
+  $dsn ='mysql:dbname=test;host=localhost;charset=utf8';
   $user = 'root';
   $password = 'root';
   $dbh = new PDO($dsn, $user, $password);
   $dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
-  $sql = 'SELECT name, price, gazou FROM mst_product WHERE code=?';
+  $sql = 'SELECT test_date, test_type FROM test_time WHERE id=?';
   $stmt = $dbh->prepare($sql);
-  $data[] = $pro_code;
+  $data[] = $test_id;
   $stmt->execute($data);
 
   $rec = $stmt->fetch(PDO::FETCH_ASSOC);
-  $pro_name = $rec['name'];
-  $pro_price = $rec['price'];
-  $pro_gazou_name_old = $rec['gazou'];
+  $test_date = $rec['test_date'];
+  $test_type = $rec['test_type'];
 
   $dbh = null;
-
-  if($pro_gazou_name_old == ''){
-    $disp_gazou = '';
-  } else{
-    $disp_gazou = '<img src="./gazou/'.$pro_gazou_name_old.'">';
-  }
 
 }
 catch(Exeption $e){
@@ -53,30 +47,32 @@ catch(Exeption $e){
   exit();
 }
  ?>
-
-商品修正<br>
 <br>
-商品コード<br>
-<?php print $pro_code; ?>
+テスト情報修正<br>
+<br>
+（変更前）<br>
+テスト実施日<br>
+<?php print $test_date; ?>
 <br>
 <br>
-<form method="post" action="pro_edit_check.php" enctype="multipart/form-data">
-<input type="hidden" name="code" value="<?php print $pro_code; ?>">
-<input type="hidden" name="gazou_name_old" value="<?php print $pro_gazou_old; ?>">
-商品名<br>
-<input type="text" name="name" style="width:200px" value="<?php print $pro_name; ?>"><br><br>
-価格<br>
-<input type="text" name="price" style="width:100px"><br>
-<?php print $disp_gazou; ?>
+テスト種別<br>
+<?php print $test_type; ?>
 <br>
-画像を選んでください<br><br>
-<input type="file" name="gazou" style="width:400px"><br><br>
+<br>
+（変更後）<br>
+<form method="post" action="test_time_edit_check.php">
+テスト実施日を選んでください<br>
+<?php pulldown_year(); ?>年　<?php pulldown_month(); ?>月 　<?php pulldown_day(); ?>日<br>
+<br>
+テスト種別を選んでください<br>
+<input name="test_type" type="radio" value="前期中間テスト" checked>前期中間テスト<br>
+<input type="radio" name="test_type" value="前期期末テスト">前期期末テスト<br>
+<input type="radio" name="test_type" value="後期中間テスト">後期中間テスト<br>
+<input type="radio" name="test_type" value="後期期末テスト">後期期末テスト<br>
+<br>
 <input type="button" onclick="history.back()" value="戻る">
 <input type="submit" value="OK">
 </form>
-
-
-
 
 </body>
 </html>
