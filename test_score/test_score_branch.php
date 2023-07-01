0fbfbb176deaefc13858b8c38bba985608055a34
ryoichi <ryo.nakamura1@gmail.com>
2023-07-01 11:42:54 +0900

    branchの整備

--- test_score/test_score_branch.php
+++ test_score/test_score_branch.php
@@ -3,45 +3,45 @@ session_start ();
 session_regenerate_id(true);
 if(isset($_SESSION['login']) == false){
   print 'ログインされていません';
-  print '<a href="../staff_login/staff_login.html">ログイン画面へ</a>';
+  print '<a href="../login_member/teacher_login.html">ログイン画面へ</a>';
   exit();
 }
 
 if(isset($_POST['disp']) == true){
-    if(isset($_POST['procode']) == false){
-      header('Location:pro_ng.php');
+    if(isset($_POST['test_score_id']) == false){
+      header('Location:test_score_ng.php');
       exit();
     }
 
-    $pro_code = $_POST['procode'];
-    header('Location:pro_disp.php?procode='.$pro_code);
+    $test_score_id = $_POST['test_score_id'];
+    header('Location:test_score_disp.php?test_score_id='.$test_score_id);
     exit();
 }
 
 if(isset($_POST['add']) == true){
-  header('Location:pro_add.php');
+  header('Location:test_score_add.php');
   exit();
 }
 
 if(isset($_POST['edit']) == true){
 
-    if(isset($_POST['procode']) == false){
-      header('Location:pro_ng.php');
+    if(isset($_POST['test_score_id']) == false){
+      header('Location:test_score_ng.php');
       exit();
     }
-  $pro_code = $_POST['procode'];
-  header('Location:pro_edit.php?procode='.$pro_code);
+  $test_score_id = $_POST['test_score_id'];
+  header('Location:test_score_edit.php?test_score_id='.$test_score_id);
   exit();
 }
 
 if(isset($_POST['delete']) == true){
 
-    if(isset($_POST['procode']) == false){
-      header('Location:pro_ng.php');
+    if(isset($_POST['test_score_id']) == false){
+      header('Location:test_score_ng.php');
       exit();
     }
-  $pro_code = $_POST['procode'];
-  header('Location:pro_delete.php?procode='.$pro_code);
+  $test_score_id = $_POST['test_score_id'];
+  header('Location:test_score_delete.php?test_score_id='.$test_score_id);
   exit();
 }
 
