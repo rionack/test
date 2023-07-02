fa031f7eab66be8f018360dec2dfb27c076c0a2a
ryoichi <ryo.nakamura1@gmail.com>
2023-07-03 08:50:52 +0900

    改造

--- test_score/test_score_delete.php
+++ test_score/test_score_delete.php
@@ -3,11 +3,11 @@ session_start ();
 session_regenerate_id(true);
 if(isset($_SESSION['login']) == false){
   print 'ログインされていません';
-  print '<a href="../staff_login/staff_login.html">ログイン画面へ</a>';
+  print '<a href="../login_member/teacher_login.html">ログイン画面へ</a>';
   exit();
 } else{
-  print $_SESSION['staff_name'];
-  print 'さんがログイン中<br>';
+  print $_SESSION['teacher_name'];
+  print '先生がログイン中<br>';
 }
  ?>
 
@@ -15,61 +15,76 @@ if(isset($_SESSION['login']) == false){
 <html>
 <head>
 <meta charset="UTF-8">
-<title>ろくまる農園</title>
+<title>成績管理アプリ</title>
 </head>
 <body>
 <?php
 try{
 
-  $pro_code = $_GET['procode'];
+  $test_score_id = $_GET['test_score_id'];
 
-  $dsn ='mysql:dbname=shop;host=localhost;charset=utf8';
+  $dsn ='mysql:dbname=test;host=localhost;charset=utf8';
   $user = 'root';
   $password = 'root';
   $dbh = new PDO($dsn, $user, $password);
   $dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
-  $sql = 'SELECT name, price, gazou FROM mst_product WHERE code=?';
+  $sql = 'SELECT ts.id, ts.student_code, ts.koku, ts.suu, ts.sya, ts.ri, ts.ei, tt.test_date, tt.test_type, s.name
+            FROM test_score ts
+            LEFT JOIN test_time tt ON ts.test_id = tt.id
+            LEFT JOIN students s ON ts.student_code = s.student_code
+            WHERE ts.id=?';
   $stmt = $dbh->prepare($sql);
-  $data[] = $pro_code;
+  $data[] = $test_score_id;
   $stmt->execute($data);
 
   $rec = $stmt->fetch(PDO::FETCH_ASSOC);
-  $pro_name = $rec['name'];
-  $pro_price = $rec['price'];
-  $pro_gazou_name = $rec['gazou'];
+  $student_code = $rec['student_code'];
+  $koku = $rec['koku'];
+  $suu = $rec['suu'];
+  $sya = $rec['sya'];
+  $ri = $rec['ri'];
+  $ei = $rec['ei'];
+  $test_date = $rec['test_date'];
+  $test_type = $rec['test_type'];
+  $student_name = $rec['name'];
 
-  $dbh = null;
-
-   if($pro_gazou_name == ''){
-     $disp_gazou = '';
-   } else{
-     $disp_gazou = '<img src="./gazou/'.$pro_gazou_name.'">';
-   }
+    $dbh = null;
 
 }
-catch(Exeption $e){
+catch(Exception $e){
   print '障害が発生しておりご迷惑をおかけいたします';
   exit();
 }
  ?>
 
-商品削除<br>
+テスト点数データ削除<br>
 <br>
-商品コード<br>
-<?php print $pro_code; ?>
+学生番号<br>
+<?php print $student_code; ?>
 <br>
-商品名<br>
-<?php print $pro_name; ?>
+生徒名<br>
+<?php print $student_name; ?>
 <br>
-価格<br>
-<?php print $pro_price . '円<br>'; ?>
-<?php print $disp_gazou; ?>
+テスト実施日<br>
+<?php print $test_date; ?>
+テスト種別<br>
+<?php print $test_type; ?>
 <br>
-この商品を削除してよろしいですか？<br><br>
-<form method="post" action="pro_delete_done.php">
-<input type="hidden" name="code" value="<?php print $pro_code; ?>">
-<input type="hidden" name="gazou_name" value="<?php print $pro_gaou_name; ?>">
+国語<?php print $koku; ?>点
+<br>
+数学<?php print $suu; ?>点
+<br>
+社会<?php print $sya; ?>点
+<br>
+理科<?php print $ri; ?>点
+英語<br>
+<?php print $ei; ?>点
+
+<br>
+この点数データを削除してよろしいですか？<br><br>
+<form method="post" action="test_score_delete_done.php">
+<input type="hidden" name="test_score_id" value="<?php print $test_score_id; ?>">
 <input type="button" onclick="history.back()" value="戻る">
 <input type="submit" value="OK">
 </form>
