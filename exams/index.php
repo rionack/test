1e77d4628d4119577894c5da48351134f6b40f3e
ryoichi <ryo.nakamura1@gmail.com>
2023-07-09 10:37:56 +0900

    no message

--- exams/index.php
+++ exams/index.php
@@ -52,9 +52,10 @@ foreach ($rec as $test) {
 <a href="../login_member/teacher_top.php">トップメニューへ</a><br>
 
 <?php
-  if(isset($_GET['test_id2']) == false){
+  if(!isset($_GET['test_id2'])){
     exit();
-  }else{
+  }
+
   $test_id2 = $_GET['test_id2'];
 
   $dsn = 'mysql:dbname=test;host=localhost;charset=utf8';
@@ -63,59 +64,29 @@ foreach ($rec as $test) {
   $dbh = new PDO($dsn, $user, $password);
   $dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
-  $orderByStudent = $_GET['student_order'];
-  $orderByName = $_GET['name_order'];
-  $orderByEi = $_GET['ei_order'];
-
-  if(isset($orderByStudent) == true){
-    $order_rule = 'ts.student_code';
-    $asc_desc = $orderByStudent;
-  }
-  elseif(isset($orderByName) == true){
-    $order_rule = 's.name';
-    $asc_desc = $orderByName;
-  }
-  elseif(isset($orderByEi) == true){
-    $order_rule = 'ts.ei';
-    $asc_desc = $orderByEi;
-  }
-  else{
-    exit();
-  }
-
 
   $sql = 'SELECT ts.id, ts.student_code, ts.koku, ts.suu, ts.sya, ts.ri, ts.ei, s.name, t.test_date, t.test_type
           FROM test_score ts
           LEFT JOIN students s ON ts.student_code = s.student_code
           LEFT JOIN test_time t ON ts.test_id = t.id
-          WHERE t.id=?
-          ORDER BY '.$order_rule.' '.$asc_desc;
+          WHERE t.id=?';
 
   $stmt = $dbh->prepare($sql);
   $stmt->execute([$test_id2]);
-  $tests = $stmt->fetchAll(PDO::FETCH_ASSOC);
-
+  $tests = $stmt->fetchALL(PDO::FETCH_ASSOC);
   $dbh = null;
-}
+
 ?>
 <table border="1" style="border-collapse: collapse">
   <tr>
     <td>
       学生番号
-      <?php
-      $sort_link = 'result.php?test_id=' .$test_id2 . '&student_order=ASC';
-      print '<a href="' . $sort_link . '&student_order=ASC">▽</a><a href="' . $sort_link . '&student_order=DESC">△</a>';
-      ?>
     </td>
     <td>
       名前
-      <?php print '<a href="' . $sort_link . '&name_order=ASC">▽</a><a href="' . $sort_link . '&name_order=DESC">△</a>';
-      ?>
     </td>
     <td>
       英語
-      <?php print '<a href="' . $sort_link . '&ei_order=ASC">▽</a><a href="' . $sort_link . '&ei_order=DESC">△</a>';
-      ?>
     </td>
     <td>数学</td>
     <td>国語</td>
